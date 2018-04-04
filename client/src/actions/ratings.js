4ffab50e6ad475bf07f83a933686923726759c4f
Regan <rmeloche@gasbuddy.com>
2018-04-03 21:43:14 -0600

    debug

--- client/src/actions/ratings.js
+++ client/src/actions/ratings.js
@@ -28,9 +28,6 @@ export function fetchRatings() {
   const request = axios({
     method:'get',
     url:'/api/rating',
-    headers: {
-      Cookie: 'cookie1=test',
-    },
   });
   //const request = axios.get(`/api/rating`);
   return {
