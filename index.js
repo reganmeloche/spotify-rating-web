83700400a51191be1abe45242f2c3cd0685ee543
Regan <rmeloche@gasbuddy.com>
2018-04-03 18:01:15 -0600

    debug

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express();
 // Setup proxy
 app.use('/', proxy(keys.serverHost, {
   filter: (req) => {
+    console.log('FILTERING', Object.keys(req), req.path, keys.serverHost);
     return (req.path.indexOf('/auth') === 0) ||(req.path.indexOf('/api') === 0);
   }
 }));
