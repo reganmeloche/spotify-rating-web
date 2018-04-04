eb008ec00115486be885d1f2f8e36297379961d7
Regan <rmeloche@gasbuddy.com>
2018-04-03 22:18:16 -0600

    debug

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -5,7 +5,7 @@ export const LOGOUT = 'logout';
 
 export function fetchUser() {
   console.log('FETCHING USER...');
-  const request = axios.({
+  const request = axios({
     method: 'get',
     url: `/api/user`,
     withCredentials: true,
