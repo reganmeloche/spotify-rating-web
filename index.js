c0e9ea3d937bd0e84b40acb7d2ba61368281e962
Regan Meloche <reganmeloche@gmail.com>
2018-04-07 23:43:23 -0600

    debug

--- index.js
+++ index.js
@@ -14,20 +14,6 @@ app.set('port', (process.env.PORT || 7107));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Prod setup
-if (process.env.NODE_ENV === 'production') {
-  // express serves up production assets (main.js, main.css, etc)
-  // if any get request comes in and we don't have a handler,
-  // then look into that directory and see if we can find it
-  app.use(express.static('client/build'));
-
-  // express serves up index.html if it doesn't recognize the route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 app.use('/api/*', (req, res, next) => {
   console.log('middleware cookie: ', req.headers.cookie);
   if (myCookie) {
@@ -55,6 +41,20 @@ app.use('/', proxy(keys.serverHost, {
     return (req.path.indexOf('/auth') === 0) ||(req.path.indexOf('/api') === 0);
   }
 }));
+
+// Prod setup
+if (process.env.NODE_ENV === 'production') {
+  // express serves up production assets (main.js, main.css, etc)
+  // if any get request comes in and we don't have a handler,
+  // then look into that directory and see if we can find it
+  app.use(express.static('client/build'));
+
+  // express serves up index.html if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // Start the server
 let server = app.listen(app.get('port'), function () {
